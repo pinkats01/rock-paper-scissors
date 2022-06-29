daf8f18ba9b9e91b97c1bed83b1e9400063d26ed
khadija el mouden <pinkats18@gmail.com>
2022-06-29 19:48:28 +0100

    upload index.html

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
-        let things= ["Rock", "Paper", "Scissors"];
+/*let things= ["Rock", "Paper", "Scissors"];
 
         let playerScore= 0;
         let computerScore= 0;
@@ -41,7 +31,4 @@
                console.log(playRound());
             }
         }
-        console.log(game());
-    </script>
-</body>
-</html>
+        console.log(game());*/
